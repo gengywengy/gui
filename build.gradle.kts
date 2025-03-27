a8af4ab2de664ddfedf67560b80634c795c9dede
Gengar <gengywengy@proton.me>
2025-03-27 15:30:54 -0700

    Created project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "dev.gengy"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
+            name = "spigotmc-repo"
+        }
+        maven("https://oss.sonatype.org/content/groups/public/") {
+            name = "sonatype"
+        }
+        mavenLocal()
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+rootProject.name = "gui"
+
+include("api")
+include(
+    "spigot:common",
+//    "spigot:1_20_R1",
+//    "spigot:1_20_R2",
+//    "spigot:1_20_R3",
+//    "spigot:1_20_R4",
+    "spigot:spigot_1_21_R1",
+    "spigot:spigot_1_21_R2"
+)
+
