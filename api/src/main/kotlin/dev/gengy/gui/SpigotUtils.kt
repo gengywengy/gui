5b37af76574db89eb59cf259ff6d82baceb0fec9
Gengar <gengywengy@proton.me>
2025-03-27 16:34:11 -0700

    Added openInventory extension

--- api/src/main/kotlin/dev/gengy/gui/SpigotUtils.kt
+++ api/src/main/kotlin/dev/gengy/gui/SpigotUtils.kt
@@ -1,7 +1,8 @@
 package dev.gengy.gui
 
 import org.bukkit.entity.Player
+import org.bukkit.inventory.InventoryView
 
-fun Player.openInventory(inventory: Inventory) {
-
+fun Player.openInventory(inventory: BasicInventory): InventoryView? {
+    return inventory.show(this)
 }
